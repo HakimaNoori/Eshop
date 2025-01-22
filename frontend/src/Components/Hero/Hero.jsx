2e15546843eceafc7547df33f7bca5349f78dbc6
HakimaNoori <hakima.noori.1379@gmail.com>
2025-01-22 21:07:38 +0430

    Add Button file in to Hero folder.

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from "react-slick";
 import Image1 from "../../assets/hero/headphone.png"
 import Image2 from "../../assets/hero/watch.png"
-
+import Button from '../Shored/Button';
 const HeroData = [
     {
         id: 1,
@@ -41,44 +41,51 @@ const Hero = () => {
         pauseOnFocus: true,
     };
   return (
-      <div className='container'>
-          <div className='overflow-hidden rounded-3xl min-h-[550px]  sm:min-h-[650px] hero-bg-color'>
-            <div className='container pb-8 sm:pb-0'>
-              {/* Hero section */}
-              <Slider {...settings}>
-                  {
-                    HeroData.map((data) => (
-                        <div key={data.id}>
-                            <div className='grid grid-cols-1 sm:grid-cols-2'>
-                                {/* text content section */}
-                                <div>
-                                    <h1>{ data.subtitie}</h1>
-                                    <h1>{ data.title}</h1>
-                                    <h1>{data.title2}</h1>
-                                    <div>
-                                        <button>Shop Now</button>
-                                    </div>
-                                  </div>
-                                  {/* Img section */}
-                                  <div>
-                                      <div>
-                                          <img
-                                              src={data.img}
-                                              alt=""
-                                              className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]'
-                                          />
-                                      </div>
-                                  </div>
-                              </div>
-                          </div> 
-                      ))
-                  }
-                  
-                </Slider>
-            </div>
-          </div>
+    <div className="container">
+      <div className="overflow-hidden rounded-3xl min-h-[550px]  sm:min-h-[650px] hero-bg-color flex justify-center items-center ">
+        <div className="container pb-8 sm:pb-0">
+          {/* Hero section */}
+          <Slider {...settings}>
+            {HeroData.map((data) => (
+              <div key={data.id}>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                  {/* text content section */}
+                  <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                    <h1 className="text-2xl sm:text-6xl lg:text-2xl font-bold">
+                      {data.subtitie}
+                    </h1>
+                    <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                      {data.title}
+                    </h1>
+                    <h1 className="text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold">
+                      {data.title2}
+                    </h1>
+                    <div>
+                      <Button
+                        text="Shop By Category"
+                        bgColor="bg-primary"
+                        textColor="text-white"
+                      />
+                    </div>
+                  </div>
+                  {/* Img section */}
+                  <div className="order-1 sm:order-2">
+                    <div>
+                      <img
+                        src={data.img}
+                        alt=""
+                        className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40"
+                      />
+                    </div>
+                  </div>
+                </div>    
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero
